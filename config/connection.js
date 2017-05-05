b0a36ffe7ce0d389a660be4666bd6c385bda3eb9
navarretejuan <navarretejuan09@gmail.com>
2017-05-05 15:22:45 -0700

    change connection

--- config/connection.js
+++ config/connection.js
@@ -1,37 +1,13 @@
 /**
  * Created by juancarlosnavarrete on 5/1/17.
  */
-// var mysql = require("mysql");
-// var connection;
-//
-//
-//
-// if (process.env.JAWSDB_URL) {
-//     connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-//     connection = mysql.createConnection({
-//         host: "localhost",
-//         user: "root",
-//         password: "",
-//         database: "burgers_db"
-//     });
-// };
-//
-// connection.connect(function(err) {
-//     if (err) {
-//         console.error('error connecting: ' + err.stack);
-//         return;
-//     }
-//     console.log("connected as id " + connection.threadId);
-// });
-//
-// module.exports= connection;
+
 
 
 var mysql = require('mysql');
 var connection;
 
-// For Heroku Deployment vs. Local MySQL Database
+
 if(process.env.JAWSDB_URL){
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 }
